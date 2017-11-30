03c895263abe2000fb36336fd8fb0604ef5c449d
tvarderesyan <colovarderesyan@yahoo.com>
2017-11-30 21:43:05 +0300

    Initial commit

---
+++ src/main/java/Hello_World.java
@@ -0,0 +1,9 @@
+/**
+ * Created by MY on 29.11.2017.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+// Just printing of text "Hello, world!"
+        System.out.println("Hello, world!");
+    }
+}

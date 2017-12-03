4a70317a647362e5a8de77c709352eb5e7cbf295
tvarderesyan <colovarderesyan@yahoo.com>
2017-12-03 18:55:53 +0300

    Finish

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+/**
+ * Created by MY on 29.11.2017.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner Calc = new Scanner(System.in);
+        double num1;
+        double num2;
+        double result;
+        char op = 0;
+
+        System.out.println("Enter the first number: ");
+        num1 = Calc.nextDouble();
+        System.out.println("Enter the second number:");
+        num2 = Calc.nextDouble();
+        System.out.println("Select the order of operation: ");
+        op = Calc.next().charAt(0);
+        switch (op) {
+            case '+':
+                System.out.println("Result: ");
+                System.out.printf("%.4f", (num1 + num2), Math.PI);
+                break;
+            case '-':
+                System.out.println("Result: ");
+                System.out.printf("%.4f", (num1 - num2), Math.PI);
+                break;
+            case '*':
+                System.out.println("Result: ");
+                System.out.printf("%.4f", (num1 * num2), Math.PI);
+                break;
+            case '/':
+                System.out.println("Result: ");
+                System.out.printf("%.4f", (num1 / num2), Math.PI);
+                break;
+        }
+    }
+
+}
--- src/main/java/Hello_World.java
+++ src/main/java/Hello_World.java
@@ -1,7 +1,7 @@
 /**
- * Created by MY on 29.11.2017.
+ * Created by MY on 03.12.2017.
  */
-public class Calculator {
+public class Hello_World {
     public static void main(String[] args) {
 // Just printing of text "Hello, world!"
         System.out.println("Hello, world!");
